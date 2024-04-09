e9a3995d2a3e8d3d8abae8b0ef1e44a21ed5bfa3
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-04-09 14:11:57 +0200

    commit-8

--- src/Components/App/App.jsx
+++ src/Components/App/App.jsx
@@ -55,7 +55,6 @@ function App() {
 			)}
 			{isLoading && <Loader />}
 			{error && <ErrorMessage message={error} />}
-			{/* <ImageModal /> */}
 			<LoadMoreBtn onClick={fetchImages} />
 		</div>
 	);
