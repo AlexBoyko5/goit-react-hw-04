4a79638be33f1dce575a827ffd822e34cb3ebdd7
AlexBoyko5 <144361285+AlexBoyko5@users.noreply.github.com>
2024-04-17 10:13:22 +0200

    commit-18
    Add css

--- src/components/LoadMoreBtn/LoadMoreBtn.jsx
+++ src/components/LoadMoreBtn/LoadMoreBtn.jsx
@@ -1,4 +1,9 @@
+import styles from './LoadBtnTemp.module.css';
 const LoadMoreBtn = ({ onClick }) => {
-	return <button onClick={onClick}>LoadMore</button>;
+	return (
+		<button onClick={onClick} className={styles.LoadMoreBtn}>
+			LoadMore
+		</button>
+	);
 };
 export default LoadMoreBtn;
